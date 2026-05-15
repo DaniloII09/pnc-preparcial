3b5db06608c33970e9639b21764f0da85ce49f6e
DaniloII09 <00377223@uca.edu.sv>
2026-05-14 18:17:35 -0600

    feat: project initial config

---
+++ inventario_restaurante/src/main/java/com/example/inventario_restaurante/InventarioRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventario_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioRestauranteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioRestauranteApplication.class, args);
+    }
+
+}
---
+++ inventario_restaurante/src/test/java/com/example/inventario_restaurante/InventarioRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventario_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

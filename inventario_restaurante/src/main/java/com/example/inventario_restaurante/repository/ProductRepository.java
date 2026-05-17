c7981466b4be9d53ef0db0047659ce51ef553d8d
DaniloII09 <00377223@uca.edu.sv>
2026-05-17 00:07:03 -0600

    add: derived query methods to ProductRepository

--- inventario_restaurante/src/main/java/com/example/inventario_restaurante/repository/ProductRepository.java
+++ inventario_restaurante/src/main/java/com/example/inventario_restaurante/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.example.inventario_restaurante.repository;
 
 import com.example.inventario_restaurante.model.entity.Product;
+import com.example.inventario_restaurante.model.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByNameIgnoreCase(String name);
+    List<Product> findByCategoryAndAvailable(Category category, Boolean available);
 }

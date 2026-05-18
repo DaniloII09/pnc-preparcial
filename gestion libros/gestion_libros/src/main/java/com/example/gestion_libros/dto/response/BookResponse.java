a50bb934f8265eb9b0c81150280d66bf7e545cc4
DaniloII09 <00377223@uca.edu.sv>
2026-05-17 20:40:06 -0600

    fix: resolving missing field in book response

--- gestion libros/gestion_libros/src/main/java/com/example/gestion_libros/dto/response/BookResponse.java
+++ gestion libros/gestion_libros/src/main/java/com/example/gestion_libros/dto/response/BookResponse.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class BookResponse {
+    private Long id;
     private String title;
     private String author;
     private String isbn;

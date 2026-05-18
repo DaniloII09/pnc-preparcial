d7fd96684133cf831c7af5b05581b04400fd9e08
DaniloII09 <00377223@uca.edu.sv>
2026-05-17 19:56:50 -0600

    add: exceptions and exception handler

---
+++ gestion libros/gestion_libros/src/main/java/com/example/gestion_libros/exception/DuplicateEntityException.java
@@ -0,0 +1,11 @@
+package com.example.gestion_libros.exception;
+
+public class DuplicateEntityException extends RuntimeException {
+    public DuplicateEntityException(String message) {
+        super(message);
+    }
+
+    public DuplicateEntityException(String entityName, String field, Object value) {
+        super(entityName + " already exists with " + field + ": " + value);
+    }
+}
